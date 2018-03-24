a5997d41bcba15a1466b82e087ddef53467cc0b7
Ciaran Deegan <ciarandee@gmail.com>
2018-03-24 13:44:01 -0400

    moved template code into methods

--- public/components/Grid.vue
+++ public/components/Grid.vue
@@ -3,12 +3,12 @@
     <h1 class="title">thisopenspace</h1>
     <h2 class="subtitle is-6">has <strong>{{spaces.length}}</strong> spaces to show you</h2>
 
-    <div class="columns" v-for="i in Math.ceil(pagesVisible * spacesPerPage / spacesPerRow)" :key="i">
-        <cell class="column" v-for="space of spaces.slice((i - 1) * spacesPerRow, i * spacesPerRow)" :key="space.id" v-bind:space="space">
+    <div class="columns" v-for="i in calcRowCount()" :key="i">
+        <cell class="column is-one-third" v-for="space of getRowItems(i)" :key="space.id" v-bind:space="space">
         </cell>
     </div>
     <div class="has-text-centered">
-    <a class="button is-primary" v-if="spaces.length > pagesVisible * spacesPerPage" v-on:click="showMoreSpaces()">Show More</a>
+    <a class="button is-primary" v-if="hasMoreItems()" v-on:click="showMoreSpaces()">Show More</a>
     </div>
 </div>
 </template>
@@ -25,17 +25,18 @@ module.exports = {
             spaces: [],
             queryPageNum: 1,
             pagesVisible: 1,
-            spacesPerPage: 9,
             spacesPerRow: 3
         }
     },
-    mounted() {
+    created() {
+        //get spaces when data ready (DOM not ready yet, but also not required for this action)
         this.getSpaces(this.queryPageNum, this.spaces, function(spaces) {
             console.log(spaces);
         })
     },
     methods: {
         getSpaces: function getSpaces(pageNum, spaces, callback) {
+            //recursively get spaces from thisopenspace's endpoint
             HTTPClient.get(baseURL + "?page=" + pageNum, function(status, response){
                 if(status == 200) {
                     spaces.push.apply(spaces, JSON.parse(response).data);
@@ -47,7 +48,20 @@ module.exports = {
                 }
             });
         },
+        getRowItems: function(i) {
+            //returns a slice of the array corresponding to the row number (i)
+            return this.spaces.slice((i - 1) * this.spacesPerRow, i * this.spacesPerRow)
+        },
+        calcRowCount: function() {
+            //returns the amount of visible rows
+            return Math.ceil(this.pagesVisible * Math.pow(this.spacesPerRow, 2) / this.spacesPerRow);
+        },
+        hasMoreItems: function() {
+            //check amount of spaces available against spaces currently shown to determine if more can be shown
+            return this.spaces.length > this.pagesVisible * Math.pow(this.spacesPerRow, 2);
+        },
         showMoreSpaces: function() {
+            //increment page number
             this.pagesVisible++;
         }
     },
